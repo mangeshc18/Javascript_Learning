f078a71e0c3b14467788593f52066933021759a8
mangeshc18 <mchoudhari861@gmail.com>
2023-12-05 17:52:12 +0530

    conditional statement

--- Lesson_2 Conditional St/conditional statement.js
+++ Lesson_2 Conditional St/conditional statement.js
@@ -2,6 +2,204 @@
 
 //aritmetic operators in js
 
+// let a = 4;
+// let b = 5;
+
+// console.log("a + b =", a+b);
+// console.log("a - b =", a-b);
+// console.log("a / b =", a/b);
+// console.log("a * b =", a*b);
+
+
+//modules
+
+// console.log("a % b =", a%b);
+
+
+//exponention operators
+
+// console.log("a**b =", a**b);
+
+// console.log("a*b =", a*b);
+// console.log("a**b =", a**b);
+
+//unary operators
+/*
 let a = 4;
 let b = 5;
+
+
+console.log("a++ =", a++);
+console.log(a);
+console.log("++a =", ++a);
+console.log(a);
+
+console.log("a+1 =", a+1);
+console.log("++a =", ++a);
+console.log("a-1 =", a-1);
+console.log("a-- =", a--);
+console.log(a);
+console.log("--a =", --a);
+console.log(a);
+*/
+
+
+
+//assignment operators
+
+// let a = 20;
+// let b = 10;
+
+// console.log("a+b =", a+b );
+// console.log("a += b =", a+=b ); //22
+// console.log("a -= b =", a-b ); //
+// console.log("a % b =" , a%b);
+// console.log("a %= b =", a%=b);
+// console.log("a x= b =", ax=b);
+
+
+//comparison operators
+
+
+// console.log("a == b", a==b);//true
+// console.log("a == b", a==b);//true
+// console.log("a === b", a===b);//false because b is a string
+// console.log("a !== b", a!==b);//false because b is a string
+
+/*
+console.log("a>b =", a>b);//false
+console.log("a>=b =", a>=b);//false
+console.log("a<b =", a<b);//false
+console.log("a<=b =", a<=b);//false
+*/
+
+
+
+//logical operators
+
+// console.log("a > b && b > a =", a>b && b>a);
+// console.log("a=b && b<a =", a=b&&b<=a);//in this case both conditions are same thats why it shows true
+// console.log("a=b && b<a =", a=b&&b>=a);//in this case both conditions are same thats why it shows true
+
+//logical or
+
+// let a = 4;
+// let b = 6;
+// console.log("a=b || b = a =",a=b||b=c);
+
+// function A() {
+//     console.log("called A");
+//     return true;
+//   }
+//   function B() {
+//     console.log("called B");
+//     return false;
+//   }
+  
+//   console.log(B() || A());
+
+
+
+//conditional Statements
+
+// let color;
+// if(mode === "dark-mode") {
+//     color = "black";
+// }else{
+//     color = "white";
+// };
+
+
+// let age = prompt("when mangesh was born?")
+
+// if( age == 1996) {
+//     alert("You are right")
+// }else{
+//     alert("you are wrong")
+// };
+
+
+// let color;
+// let mode = ""
+
+// if(mode === "dark"){
+//     console.log(color = "black");
+// }else{
+//     console.log(color = "white");
+// }
+
+
+// let value = prompt('What is the "official" name of JavaScript?', '');
+
+//     if (value == 'ECMAScript') {
+//       alert('Right!');
+//     } else {
+//       alert("You don't know? ECMAScript!");
+//     }
+
+
+
+
+
+
+// let value = prompt('Type a number', 0);
+
+// if (value > 0) {
+//   alert( 1 );
+// } else if (value < 0) {
+//   alert( -1 );
+// } else {
+//   alert( 0 );
+// }
+
+
+// let message;
+// let login = ""
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+
+// console.log(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
